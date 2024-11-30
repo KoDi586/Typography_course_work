175f3414f973a521d20c834aaf1de57b0127a218
Kodi <hirofool502@gmail.com>
2024-11-30 14:22:03 +0300

    beginning

---
+++ Typography_course_work/src/main/java/com/example/Typography_course_work/TypographyCourseWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.Typography_course_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypographyCourseWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypographyCourseWorkApplication.class, args);
+	}
+
+}
---
+++ Typography_course_work/src/test/java/com/example/Typography_course_work/TypographyCourseWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Typography_course_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypographyCourseWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
